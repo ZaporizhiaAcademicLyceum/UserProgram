4709b1374a8e0e3d6f583b887e7e097112f545f4
Alexander <snosme@gmail.com>
2017-05-01 18:19:41 +0300

    Add project files.

---
+++ SchoolService/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SchoolService
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            statusValue.Content = Environment.MachineName + "\n" +
+                Environment.UserName + "\n" +
+                counter;
+            counter++;
+            HttpRequest();
+        }
+
+        private void HttpRequest()
+        {
+            string html = string.Empty;
+
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("https://api.master-stvo.com/");
+            using (HttpWebResponse response = (HttpWebResponse)myReq.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+            Console.WriteLine(html);
+        }
+    }
+}
